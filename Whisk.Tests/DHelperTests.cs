b2a9b8b3a68afc1539f2bd8f33da031c8848aa42
John Gietzen <john@gietzen.us>
2020-05-15 17:31:37 -0700

    Added more tests covering the Set method.

--- Whisk.Tests/DHelperTests.cs
+++ Whisk.Tests/DHelperTests.cs
@@ -9,6 +9,86 @@ namespace Whisk.Tests
 
     public class DHelperTests
     {
+        public static readonly object[][] TestValues =
+        {
+            new object[] { 0 },
+            new object[] { 1 },
+            new object[] { 2 },
+            new object[] { -1 },
+            new object[] { 10 },
+            new object[] { 1024 },
+            new object[] { int.MaxValue },
+            new object[] { int.MinValue },
+        };
+
+        [Theory]
+        [MemberData(nameof(TestValues))]
+        public void Set_WhenGivenADistinctValue_RaisesMarkBeforeSweep(int value)
+        {
+            var d = new MutableDependency<int>(value);
+            var order = 1;
+            var marked = 0;
+            var swept = 0;
+            d.MarkInvalidated += (sender, args) => marked = order++;
+            d.SweepInvalidated += (sender, args) => swept = order++;
+
+            D.Set(D.Value(d, unchecked(value + 1)));
+
+            Assert.True(swept > marked);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestValues))]
+        public void Set_WhenGivenADistinctValue_RaisesTheMarkInvalidatedEventOnce(int value)
+        {
+            var d = new MutableDependency<int>(value);
+            var raised = 0;
+            d.MarkInvalidated += (sender, args) => Interlocked.Increment(ref raised);
+
+            D.Set(D.Value(d, unchecked(value + 1)));
+
+            Assert.Equal(1, raised);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestValues))]
+        public void Set_WhenGivenADistinctValue_RaisesTheSweepInvalidatedEventOnce(int value)
+        {
+            var d = new MutableDependency<int>(value);
+            var raised = 0;
+            d.SweepInvalidated += (sender, args) => Interlocked.Increment(ref raised);
+
+            D.Set(D.Value(d, unchecked(value + 1)));
+
+            Assert.Equal(1, raised);
+        }
+
+        [Fact]
+        public void Set_WhenGivenAnEqualValue_DoesNotRaiseTheMarkInvalidatedEvent()
+        {
+            var d = new MutableDependency<string>("OK", StringComparer.OrdinalIgnoreCase);
+            var raised = 0;
+            d.MarkInvalidated += (sender, args) => Interlocked.Increment(ref raised);
+
+            D.Set(D.Value(d, "ok"));
+
+            Assert.Equal(0, raised);
+            Assert.Equal("OK", d.Value);
+        }
+
+        [Fact]
+        public void Set_WhenGivenAnEqualValue_DoesNotRaiseTheSweepInvalidatedEvent()
+        {
+            var d = new MutableDependency<string>("OK", StringComparer.OrdinalIgnoreCase);
+            var raised = 0;
+            d.SweepInvalidated += (sender, args) => Interlocked.Increment(ref raised);
+
+            D.Set(D.Value(d, "ok"));
+
+            Assert.Equal(0, raised);
+            Assert.Equal("OK", d.Value);
+        }
+
         [Fact]
         public void Set_WhenGivenMultipleValues_UpdatesTheValuesAtomically()
         {
